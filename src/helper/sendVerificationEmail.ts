800d63cb641377836573e93baa3821d84d3591c1
Vivek <viveklearning2003@gmail.com>
2024-07-31 20:22:41 +0530

    email updates

--- src/helper/sendVerificationEmail.ts
+++ src/helper/sendVerificationEmail.ts
@@ -10,7 +10,7 @@ export async function sendVerificationMessage(
     console.log("Entered in verifiaction email")
     try {
             await resend.emails.send({
-            from: 'onboarding@resend.dev',
+            from: 'valo14411@gmail.com',
             to: email,
             subject: 'Hello Heres Your Verification Code',
             react: VerificationEmail({username , otp : verifyCode}),
